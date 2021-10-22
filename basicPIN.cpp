8145f5a8e87efa21606c96776709a101f49ad944
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2021-10-22 22:21:50 +0100

    Create basicPIN.cpp

---
+++ basicPIN.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	int userPin = 1234, pin, errorCounter = 0;
+	
+	do {
+		cout << "PIN: ";
+		cin >> pin;
+		if (pin != usersPin)
+			errorCounter++;
+
+	} while (errorCounter < 3 && pin != usersPin);
+
+	if (errorCounter < 3)
+		cout << "Loading....\n";
+		cout << "Correct!"
+	else 
+		cout << "Incorrect PIN. Blocked!";
+
+system("paused>0");
+}
